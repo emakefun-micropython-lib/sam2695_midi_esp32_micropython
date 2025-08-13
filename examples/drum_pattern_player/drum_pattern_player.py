90f28102e81a5da62ff791bebc280cb22c9fed1b
“xianlonluohan” <2047669296@qq.com>
2025-08-13 11:47:39 +0800

    first commit

---
+++ examples/drum_pattern_player/drum_pattern_player.py
@@ -0,0 +1,91 @@
+import time
+import urandom
+from sam2695_midi import Sam2695Midi
+from sam2695_midi_timbre import SAM2695_MIDI_TIMBRE_BANK_0
+from sam2695_midi_chorus_reverberation import SAM2695_MIDI_REVERBERATION_PLATE
+from sam2695_midi_percussion_note import (
+    SAM2695_MIDI_PERCUSSION_TIMBRE_1,
+    SAM2695_MIDI_PERCUSSION_TIMBRE_1_NOTE_C_2_KICK_DRUM_1,
+    SAM2695_MIDI_PERCUSSION_TIMBRE_1_NOTE_D_2_SNARE_DRUM_1,
+    SAM2695_MIDI_PERCUSSION_TIMBRE_1_NOTE_F_SHARP_2_CLOSED_HI_HAT,
+    SAM2695_MIDI_PERCUSSION_TIMBRE_1_NOTE_A_SHARP_2_OPEN_HI_HAT,
+    SAM2695_MIDI_PERCUSSION_TIMBRE_1_NOTE_G_SHARP_2_PEDAL_HI_HAT,
+)
+
+SAM2695_MIDI_PIN: int = 17
+
+CHANNEL: int = 9
+CHANNEL_VOLUME: int = 100
+
+REVERBERATION_VOLUME: int = 127
+REVERBERATION_DELAY_FEEDBACK: int = 100
+
+TEMPO_RANDOM_RANGE: int = 5
+TEMPO_RANDOM_OFFSET: int = 2
+
+MIN_TEMPO: int = 40
+MAX_TEMPO: int = 250
+
+TICK_SIZE: int = 15
+
+BASS_DRUM_TICK = [127, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 90, 0]
+SNARE_DRUM_TICK = [0, 0, 0, 0, 127, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+HI_HAT_OPEN_TICK = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 127, 0]
+HI_HAT_CLOSE_TICK = [127, 40, 80, 40, 127, 40, 80, 40, 127, 40, 80, 40, 127, 0, 0]
+HI_HAT_PEDAL_TICK = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 127]
+
+assert len(BASS_DRUM_TICK) == TICK_SIZE, "BASS_DRUM_TICK size must be 15"
+assert len(SNARE_DRUM_TICK) == TICK_SIZE, "SNARE_DRUM_TICK size must be 15"
+assert len(HI_HAT_OPEN_TICK) == TICK_SIZE, "HI_HAT_OPEN_TICK size must be 15"
+assert len(HI_HAT_CLOSE_TICK) == TICK_SIZE, "HI_HAT_CLOSE_TICK size must be 15"
+assert len(HI_HAT_PEDAL_TICK) == TICK_SIZE, "HI_HAT_PEDAL_TICK size must be 15"
+
+tempo: int = 120
+sam2695_midi = Sam2695Midi(tx_pin=SAM2695_MIDI_PIN)
+
+
+def play_drum_note(midi_note: int, note_velocity: int):
+    if note_velocity > 0:
+        sam2695_midi.note_on(CHANNEL, midi_note, note_velocity)
+        sam2695_midi.note_off(CHANNEL, midi_note)
+
+
+sam2695_midi.midi_reset()
+sam2695_midi.set_channel_timbre(
+    CHANNEL, SAM2695_MIDI_TIMBRE_BANK_0, SAM2695_MIDI_PERCUSSION_TIMBRE_1
+)
+sam2695_midi.set_reverberation(
+    CHANNEL,
+    SAM2695_MIDI_REVERBERATION_PLATE,
+    REVERBERATION_VOLUME,
+    REVERBERATION_DELAY_FEEDBACK,
+)
+sam2695_midi.set_channel_volume(CHANNEL, CHANNEL_VOLUME)
+
+while True:
+    for tick_no in range(TICK_SIZE):
+        tempo += urandom.getrandbits(3) % TEMPO_RANDOM_RANGE - TEMPO_RANDOM_OFFSET
+        tempo = max(MIN_TEMPO, min(tempo, MAX_TEMPO))
+
+        play_drum_note(
+            SAM2695_MIDI_PERCUSSION_TIMBRE_1_NOTE_C_2_KICK_DRUM_1,
+            BASS_DRUM_TICK[tick_no],
+        )
+        play_drum_note(
+            SAM2695_MIDI_PERCUSSION_TIMBRE_1_NOTE_D_2_SNARE_DRUM_1,
+            SNARE_DRUM_TICK[tick_no],
+        )
+        play_drum_note(
+            SAM2695_MIDI_PERCUSSION_TIMBRE_1_NOTE_F_SHARP_2_CLOSED_HI_HAT,
+            HI_HAT_CLOSE_TICK[tick_no],
+        )
+        play_drum_note(
+            SAM2695_MIDI_PERCUSSION_TIMBRE_1_NOTE_A_SHARP_2_OPEN_HI_HAT,
+            HI_HAT_OPEN_TICK[tick_no],
+        )
+        play_drum_note(
+            SAM2695_MIDI_PERCUSSION_TIMBRE_1_NOTE_G_SHARP_2_PEDAL_HI_HAT,
+            HI_HAT_PEDAL_TICK[tick_no],
+        )
+
+        time.sleep_ms(tempo)
---
+++ lib/sam2695_midi_note.py
@@ -0,0 +1,407 @@
+# MIDI note numbering definition (0-127), including all MIDI note numbering from C-1 (0) to G9 (127).
+
+# Define channel note constant, octave -1.
+
+# C-1 (note C at octave -1)
+SAM2695_MIDI_NOTE_C_M1: int = 0
+
+# C#-1 (note C sharp at octave -1)
+SAM2695_MIDI_NOTE_C_SHARP_M1: int = 1
+
+# D-1 (note D at octave -1)
+SAM2695_MIDI_NOTE_D_M1: int = 2
+
+# D#-1 (note D sharp at octave -1)
+SAM2695_MIDI_NOTE_D_SHARP_M1: int = 3
+
+# E-1 (note E at octave -1)
+SAM2695_MIDI_NOTE_E_M1: int = 4
+
+# F-1 (note F at octave -1)
+SAM2695_MIDI_NOTE_F_M1: int = 5
+
+# F#-1 (note F sharp at octave -1)
+SAM2695_MIDI_NOTE_F_SHARP_M1: int = 6
+
+# G-1 (note G at octave -1)
+SAM2695_MIDI_NOTE_G_M1: int = 7
+
+# G#-1 (note G sharp at octave -1)
+SAM2695_MIDI_NOTE_G_SHARP_M1: int = 8
+
+# A-1 (note A at octave -1)
+SAM2695_MIDI_NOTE_A_M1: int = 9
+
+# A#-1 (note A sharp at octave -1)
+SAM2695_MIDI_NOTE_A_SHARP_M1: int = 10
+
+# B-1 (note B at octave -1)
+SAM2695_MIDI_NOTE_B_M1: int = 11
+
+# Define channel note constant, octave 0.
+
+# C0 (note C at octave 0)
+SAM2695_MIDI_NOTE_C_0: int = 12
+
+# C#0 (note C sharp at octave 0)
+SAM2695_MIDI_NOTE_C_SHARP_0: int = 13
+
+# D0 (note D at octave 0)
+SAM2695_MIDI_NOTE_D_0: int = 14
+
+# D#0 (note D sharp at octave 0)
+SAM2695_MIDI_NOTE_D_SHARP_0: int = 15
+
+# E0 (note E at octave 0)
+SAM2695_MIDI_NOTE_E_0: int = 16
+
+# F0 (note F at octave 0)
+SAM2695_MIDI_NOTE_F_0: int = 17
+
+# F#0 (note F sharp at octave 0)
+SAM2695_MIDI_NOTE_F_SHARP_0: int = 18
+
+# G0 (note G at octave 0)
+SAM2695_MIDI_NOTE_G_0: int = 19
+
+# G#0 (note G sharp at octave 0)
+SAM2695_MIDI_NOTE_G_SHARP_0: int = 20
+
+# A0 (note A at octave 0)
+SAM2695_MIDI_NOTE_A_0: int = 21
+
+# A#0 (note A sharp at octave 0)
+SAM2695_MIDI_NOTE_A_SHARP_0: int = 22
+
+# B0 (note B at octave 0)
+SAM2695_MIDI_NOTE_B_0: int = 23
+
+# Define channel note constant, octave 1.
+
+# C1 (note C at octave 1)
+SAM2695_MIDI_NOTE_C_1: int = 24
+
+# C#1 (note C sharp at octave 1)
+SAM2695_MIDI_NOTE_C_SHARP_1: int = 25
+
+# D1 (note D at octave 1)
+SAM2695_MIDI_NOTE_D_1: int = 26
+
+# D#1 (note D sharp at octave 1)
+SAM2695_MIDI_NOTE_D_SHARP_1: int = 27
+
+# E1 (note E at octave 1)
+SAM2695_MIDI_NOTE_E_1: int = 28
+
+# F1 (note F at octave 1)
+SAM2695_MIDI_NOTE_F_1: int = 29
+
+# F#1 (note F sharp at octave 1)
+SAM2695_MIDI_NOTE_F_SHARP_1: int = 30
+
+# G1 (note G at octave 1)
+SAM2695_MIDI_NOTE_G_1: int = 31
+
+# G#1 (note G sharp at octave 1)
+SAM2695_MIDI_NOTE_G_SHARP_1: int = 32
+
+# A1 (note A at octave 1)
+SAM2695_MIDI_NOTE_A_1: int = 33
+
+# A#1 (note A sharp at octave 1)
+SAM2695_MIDI_NOTE_A_SHARP_1: int = 34
+
+# B1 (note B at octave 1)
+SAM2695_MIDI_NOTE_B_1: int = 35
+
+# Define channel note constant, octave 2.
+
+# C2 (note C at octave 2)
+SAM2695_MIDI_NOTE_C_2: int = 36
+
+# C#2 (note C sharp at octave 2)
+SAM2695_MIDI_NOTE_C_SHARP_2: int = 37
+
+# D2 (note D at octave 2)
+SAM2695_MIDI_NOTE_D_2: int = 38
+
+# D#2 (note D sharp at octave 2)
+SAM2695_MIDI_NOTE_D_SHARP_2: int = 39
+
+# E2 (note E at octave 2)
+SAM2695_MIDI_NOTE_E_2: int = 40
+
+# F2 (note F at octave 2)
+SAM2695_MIDI_NOTE_F_2: int = 41
+
+# F#2 (note F sharp at octave 2)
+SAM2695_MIDI_NOTE_F_SHARP_2: int = 42
+
+# G2 (note G at octave 2)
+SAM2695_MIDI_NOTE_G_2: int = 43
+
+# G#2 (note G sharp at octave 2)
+SAM2695_MIDI_NOTE_G_SHARP_2: int = 44
+
+# A2 (note A at octave 2)
+SAM2695_MIDI_NOTE_A_2: int = 45
+
+# A#2 (note A sharp at octave 2)
+SAM2695_MIDI_NOTE_A_SHARP_2: int = 46
+
+# B2 (note B at octave 2)
+SAM2695_MIDI_NOTE_B_2: int = 47
+
+# Define channel note constant, octave 3.
+
+# C3 (note C at octave 3)
+SAM2695_MIDI_NOTE_C_3: int = 48
+
+# C#3 (note C sharp at octave 3)
+SAM2695_MIDI_NOTE_C_SHARP_3: int = 49
+
+# D3 (note D at octave 3)
+SAM2695_MIDI_NOTE_D_3: int = 50
+
+# D#3 (note D sharp at octave 3)
+SAM2695_MIDI_NOTE_D_SHARP_3: int = 51
+
+# E3 (note E at octave 3)
+SAM2695_MIDI_NOTE_E_3: int = 52
+
+# F3 (note F at octave 3)
+SAM2695_MIDI_NOTE_F_3: int = 53
+
+# F#3 (note F sharp at octave 3)
+SAM2695_MIDI_NOTE_F_SHARP_3: int = 54
+
+# G3 (note G at octave 3)
+SAM2695_MIDI_NOTE_G_3: int = 55
+
+# G#3 (note G sharp at octave 3)
+SAM2695_MIDI_NOTE_G_SHARP_3: int = 56
+
+# A3 (note A at octave 3)
+SAM2695_MIDI_NOTE_A_3: int = 57
+
+# A#3 (note A sharp at octave 3)
+SAM2695_MIDI_NOTE_A_SHARP_3: int = 58
+
+# B3 (note B at octave 3)
+SAM2695_MIDI_NOTE_B_3: int = 59
+
+# Define channel note constant, octave 4.
+
+# C4 (note C at octave 4, Middle C)
+SAM2695_MIDI_NOTE_C_4: int = 60
+
+# C#4 (note C sharp at octave 4)
+SAM2695_MIDI_NOTE_C_SHARP_4: int = 61
+
+# D4 (note D at octave 4)
+SAM2695_MIDI_NOTE_D_4: int = 62
+
+# D#4 (note D sharp at octave 4)
+SAM2695_MIDI_NOTE_D_SHARP_4: int = 63
+
+# E4 (note E at octave 4)
+SAM2695_MIDI_NOTE_E_4: int = 64
+
+# F4 (note F at octave 4)
+SAM2695_MIDI_NOTE_F_4: int = 65
+
+# F#4 (note F sharp at octave 4)
+SAM2695_MIDI_NOTE_F_SHARP_4: int = 66
+
+# G4 (note G at octave 4)
+SAM2695_MIDI_NOTE_G_4: int = 67
+
+# G#4 (note G sharp at octave 4)
+SAM2695_MIDI_NOTE_G_SHARP_4: int = 68
+
+# A4 (note A at octave 4, standard pitch 440Hz)
+SAM2695_MIDI_NOTE_A_4: int = 69
+
+# A#4 (note A sharp at octave 4)
+SAM2695_MIDI_NOTE_A_SHARP_4: int = 70
+
+# B4 (note B at octave 4)
+SAM2695_MIDI_NOTE_B_4: int = 71
+
+# Define channel note constant, octave 5.
+
+# C5 (note C at octave 5)
+SAM2695_MIDI_NOTE_C_5: int = 72
+
+# C#5 (note C sharp at octave 5)
+SAM2695_MIDI_NOTE_C_SHARP_5: int = 73
+
+# D5 (note D at octave 5)
+SAM2695_MIDI_NOTE_D_5: int = 74
+
+# D#5 (note D sharp at octave 5)
+SAM2695_MIDI_NOTE_D_SHARP_5: int = 75
+
+# E5 (note E at octave 5)
+SAM2695_MIDI_NOTE_E_5: int = 76
+
+# F5 (note F at octave 5)
+SAM2695_MIDI_NOTE_F_5: int = 77
+
+# F#5 (note F sharp at octave 5)
+SAM2695_MIDI_NOTE_F_SHARP_5: int = 78
+
+# G5 (note G at octave 5)
+SAM2695_MIDI_NOTE_G_5: int = 79
+
+# G#5 (note G sharp at octave 5)
+SAM2695_MIDI_NOTE_G_SHARP_5: int = 80
+
+# A5 (note A at octave 5)
+SAM2695_MIDI_NOTE_A_5: int = 81
+
+# A#5 (note A sharp at octave 5)
+SAM2695_MIDI_NOTE_A_SHARP_5: int = 82
+
+# B5 (note B at octave 5)
+SAM2695_MIDI_NOTE_B_5: int = 83
+
+# Define channel note constant, octave 6.
+
+# C6 (note C at octave 6)
+SAM2695_MIDI_NOTE_C_6: int = 84
+
+# C#6 (note C sharp at octave 6)
+SAM2695_MIDI_NOTE_C_SHARP_6: int = 85
+
+# D6 (note D at octave 6)
+SAM2695_MIDI_NOTE_D_6: int = 86
+
+# D#6 (note D sharp at octave 6)
+SAM2695_MIDI_NOTE_D_SHARP_6: int = 87
+
+# E6 (note E at octave 6)
+SAM2695_MIDI_NOTE_E_6: int = 88
+
+# F6 (note F at octave 6)
+SAM2695_MIDI_NOTE_F_6: int = 89
+
+# F#6 (note F sharp at octave 6)
+SAM2695_MIDI_NOTE_F_SHARP_6: int = 90
+
+# G6 (note G at octave 6)
+SAM2695_MIDI_NOTE_G_6: int = 91
+
+# G#6 (note G sharp at octave 6)
+SAM2695_MIDI_NOTE_G_SHARP_6: int = 92
+
+# A6 (note A at octave 6)
+SAM2695_MIDI_NOTE_A_6: int = 93
+
+# A#6 (note A sharp at octave 6)
+SAM2695_MIDI_NOTE_A_SHARP_6: int = 94
+
+# B6 (note B at octave 6)
+SAM2695_MIDI_NOTE_B_6: int = 95
+
+# Define channel note constant, octave 7.
+
+# C7 (note C at octave 7)
+SAM2695_MIDI_NOTE_C_7: int = 96
+
+# C#7 (note C sharp at octave 7)
+SAM2695_MIDI_NOTE_C_SHARP_7: int = 97
+
+# D7 (note D at octave 7)
+SAM2695_MIDI_NOTE_D_7: int = 98
+
+# D#7 (note D sharp at octave 7)
+SAM2695_MIDI_NOTE_D_SHARP_7: int = 99
+
+# E7 (note E at octave 7)
+SAM2695_MIDI_NOTE_E_7: int = 100
+
+# F7 (note F at octave 7)
+SAM2695_MIDI_NOTE_F_7: int = 101
+
+# F#7 (note F sharp at octave 7)
+SAM2695_MIDI_NOTE_F_SHARP_7: int = 102
+
+# G7 (note G at octave 7)
+SAM2695_MIDI_NOTE_G_7: int = 103
+
+# G#7 (note G sharp at octave 7)
+SAM2695_MIDI_NOTE_G_SHARP_7: int = 104
+
+# A7 (note A at octave 7)
+SAM2695_MIDI_NOTE_A_7: int = 105
+
+# A#7 (note A sharp at octave 7)
+SAM2695_MIDI_NOTE_A_SHARP_7: int = 106
+
+# B7 (note B at octave 7)
+SAM2695_MIDI_NOTE_B_7: int = 107
+
+# Define channel note constant, octave 8.
+
+# C8 (note C at octave 8)
+SAM2695_MIDI_NOTE_C_8: int = 108
+
+# C#8 (note C sharp at octave 8)
+SAM2695_MIDI_NOTE_C_SHARP_8: int = 109
+
+# D8 (note D at octave 8)
+SAM2695_MIDI_NOTE_D_8: int = 110
+
+# D#8 (note D sharp at octave 8)
+SAM2695_MIDI_NOTE_D_SHARP_8: int = 111
+
+# E8 (note E at octave 8)
+SAM2695_MIDI_NOTE_E_8: int = 112
+
+# F8 (note F at octave 8)
+SAM2695_MIDI_NOTE_F_8: int = 113
+
+# F#8 (note F sharp at octave 8)
+SAM2695_MIDI_NOTE_F_SHARP_8: int = 114
+
+# G8 (note G at octave 8)
+SAM2695_MIDI_NOTE_G_8: int = 115
+
+# G#8 (note G sharp at octave 8)
+SAM2695_MIDI_NOTE_G_SHARP_8: int = 116
+
+# A8 (note A at octave 8)
+SAM2695_MIDI_NOTE_A_8: int = 117
+
+# A#8 (note A sharp at octave 8)
+SAM2695_MIDI_NOTE_A_SHARP_8: int = 118
+
+# B8 (note B at octave 8)
+SAM2695_MIDI_NOTE_B_8: int = 119
+
+# Define channel note constant, octave 9.
+
+# C9 (note C at octave 9)
+SAM2695_MIDI_NOTE_C_9: int = 120
+
+# C#9 (note C sharp at octave 9)
+SAM2695_MIDI_NOTE_C_SHARP_9: int = 121
+
+# D9 (note D at octave 9)
+SAM2695_MIDI_NOTE_D_9: int = 122
+
+# D#9 (note D sharp at octave 9)
+SAM2695_MIDI_NOTE_D_SHARP_9: int = 123
+
+# E9 (note E at octave 9)
+SAM2695_MIDI_NOTE_E_9: int = 124
+
+# F9 (note F at octave 9)
+SAM2695_MIDI_NOTE_F_9: int = 125
+
+# F#9 (note F sharp at octave 9)
+SAM2695_MIDI_NOTE_F_SHARP_9: int = 126
+
+# G9 (note G at octave 9, highest MIDI note)
+SAM2695_MIDI_NOTE_G_9: int = 127
